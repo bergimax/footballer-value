2a66deadedf208ed901487afd227ed040eaf46c2
Massimo Bergi <90566873+bergimax@users.noreply.github.com>
2023-12-12 00:19:23 +0100

    Add files via upload

--- test.py
+++ test.py
@@ -6,28 +6,28 @@ import numpy as np
 
 url = 'http://localhost:9696/predict'
 
-#Player: 149
+#Player: 2075
+#val expected: 80000000
 
 player = {
-  "team": "southampton_fc", 
-  "name": "stuart_armstrong", 
-  "position": "midfield-centralmidfield", 
-  "height": 183.0, 
-  "age": 31.0, 
-  "appearance": 65, 
-  "goals": 0.133294, 
-  "assists": 0.053318, 
-  "yellow_cards": 0.186611, 
-  "second_yellow_cards": 0.0, 
+  "team": "ac_milan", 
+  "name": "rafael_leão", 
+  "position": "attack-leftwinger", 
+  "height": 180.0, 
+  "age": 23.0, 
+  "appearance": 90, 
+  "goals": 0.403708, 
+  "assists": 0.363337, 
+  "yellow_cards": 0.134569, 
+  "second_yellow_cards": 0.013457, 
   "red_cards": 0.0, 
   "goals_conceded": 0.0, 
   "clean_sheets": 0.0, 
-  "minutes_played": 3376, 
-  "days_injured": 259, 
-  "games_injured": 39, 
-  "award": 8, 
-  "current_value": 15.60727,
-  "highest_value": 15.894952
+  "minutes_played": 6688, 
+  "days_injured": 237, 
+  "games_injured": 34, 
+  "award": 6, 
+  "highest_value": 18.258162
 }
 
 requests.post(url, json=player)
